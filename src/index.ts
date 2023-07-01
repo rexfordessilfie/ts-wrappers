b447f0f58ccfdc0974864be65565bca6cb3182a5
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-06-30 21:27:32 -0400

    feat: add createWrapper function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,83 @@
-export default function wrapper() {
-  console.log("hello world!");
+export default function createWrapper<CArgs extends any[], CReturn>(
+  cb: (next: Next, ...args: CArgs) => CReturn
+) {
+  return <FArgs extends CArgs, FReturn>(func: (...args: FArgs) => FReturn) => {
+    return (...args: Parameters<typeof func>) => {
+      const next = (() => func(...args)) as Parameters<typeof cb>[0];
+
+      next[FUNC] = func;
+
+      return cb(next, ...(args as any)) as CReturn extends NextReturnType
+        ? Replace<CReturn, NextReturnType, ReturnType<typeof func>>
+        : Exclude<CReturn, NextReturnType>;
+    };
+  };
 }
+
+export function createWrapperWithIntersectedArgs<CArgs extends any[], CReturn>(
+  cb: (next: Next, ...args: CArgs) => CReturn
+) {
+  return <FArgs extends CArgs, FReturn>(
+    func: (
+      ...args: FArgs extends unknown[]
+        ? TupleMerge<
+            Parameters<typeof cb> extends [infer _, ...infer Args]
+              ? Args
+              : CArgs,
+            FArgs
+          >
+        : FArgs
+    ) => FReturn
+  ) => {
+    return (...args: Parameters<typeof func>) => {
+      const next = (() => func(...args)) as Parameters<typeof cb>[0];
+
+      next[FUNC] = func;
+
+      return cb(next, ...(args as any)) as CReturn extends NextReturnType
+        ? Replace<CReturn, NextReturnType, ReturnType<typeof func>>
+        : Exclude<CReturn, NextReturnType>;
+    };
+  };
+}
+
+type Replace<T, U, V> = Exclude<T, U> | V;
+
+export const FUNC = Symbol();
+
+declare const brand: unique symbol;
+
+type Brand<T, TBrand extends string> = T & {
+  [brand]: TBrand;
+};
+
+type NextBrand = "__next_return__";
+type NextReturnType = Brand<{}, NextBrand>;
+
+type ApplyNextBrand<T> = T extends NextReturnType ? T : Brand<T, NextBrand>;
+
+type Next<ReturnType = NextReturnType> = (<
+  T = ReturnType
+>() => ApplyNextBrand<T>) & {
+  [FUNC]: (...args: any[]) => any;
+};
+
+type IsAny<T> = 0 extends 1 & T ? true : false;
+
+type Merge<U, V> = IsAny<V> extends true
+  ? U
+  : IsAny<U> extends true
+  ? V
+  : V & U;
+
+type Append<T, U> = U extends any[] ? [...U, T] : [U, T];
+
+type TupleMerge<
+  ATuple extends any[],
+  BTuple extends any[],
+  SoFar extends any[] = []
+> = ATuple extends [infer A, ...infer ARest]
+  ? BTuple extends [infer B, ...infer BRest]
+    ? TupleMerge<ARest, BRest, Append<Merge<A, B>, SoFar>>
+    : [...SoFar, ...ATuple]
+  : [...SoFar, ...BTuple];
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,123 @@
 import test from "ava";
-import wrapper from "../src";
+import createWrapper from "../src";
+
+import { expectType } from "tsd";
 
 test("has defulat export", (t) => {
-  t.truthy(wrapper);
+  t.truthy(createWrapper);
+});
+
+test("infers function types from wrapper", (t) => {
+  const doubleBinNumOp = createWrapper((next, a: number, b: number) => {
+    next();
+    return next();
+  });
+
+  let count = 0;
+
+  const addDouble = doubleBinNumOp((a, b) => {
+    count += 1;
+    return a + b;
+  });
+
+  t.is(addDouble(2, 3), 5);
+  t.is(count, 2);
+});
+
+test("rejects mismatched function argument type", (t) => {
+  const doubleBinNumOp = createWrapper((next, a: number, b: number) => {
+    next();
+    return next();
+  });
+
+  const badFunc = (a: string, b: string) => {
+    return a + b;
+  };
+
+  // This should have an error
+  doubleBinNumOp(badFunc);
+
+  t.truthy(true);
+});
+
+test("allows function args to extend wrapper args", (t) => {
+  const withId = createWrapper((next, ctx: { id?: string }, ..._: any[]) => {
+    ctx.id = "1234";
+    return next();
+  });
+
+  const goodFunc = (ctx: { id?: string; name: string }, _color: string) => {
+    return ctx;
+  };
+
+  const goodFuncWithId = withId(goodFunc);
+
+  t.deepEqual(goodFuncWithId({ name: "Rex" }, "red"), {
+    name: "Rex",
+    id: "1234",
+  });
+});
+
+test("infers wrapper return type when applied", (t) => {
+  const withRandom = createWrapper((next, ..._: any[]) => {
+    const rand = Math.random();
+
+    if (rand > 0.2) {
+      return "hi";
+    }
+
+    if (rand > 0.5) {
+      return "hello";
+    }
+
+    return next();
+  });
+
+  const greet = () => "yo" as "yo";
+
+  const greetRandomized = withRandom(greet);
+
+  expectType<"hi" | "hello" | "yo">(greetRandomized());
+  t.truthy(["hi", "hello", "yo"].includes(greetRandomized()));
+});
+
+test("can type the next return in wrapper definition", (t) => {
+  class ApiKey {
+    note: string;
+    key: string;
+
+    constructor(note: string, key: string) {
+      this.note = note;
+      this.key = key;
+    }
+  }
+
+  const sanitizedApiKey = createWrapper((next, ..._: any[]) => {
+    const result = next<ApiKey>();
+    if (!(result instanceof ApiKey)) {
+      return result;
+    }
+
+    return new Proxy(result, {
+      get(target, prop, receiver) {
+        if (prop === "key") {
+          return (
+            target.key.slice(0, 3) +
+            new Array(target.key.slice(3).length).fill("*").join("")
+          );
+        }
+
+        return Reflect.get(target, prop, receiver);
+      },
+    });
+  });
+
+  const createApiKey = (note: string, key: string) => new ApiKey(note, key);
+
+  const createApiKeySanitized = sanitizedApiKey(createApiKey);
+
+  const apiKey = createApiKeySanitized("test key", "abc1234");
+
+  t.is(apiKey.key, "abc****");
+  t.is(apiKey.note, "test key");
 });
