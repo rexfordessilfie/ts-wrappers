2ea51b6d12dcbafa77d080479c544a8d93caadd0
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-01 16:48:57 -0400

    chore: add promise to debounce

--- examples/debounce.ts
+++ examples/debounce.ts
@@ -4,22 +4,28 @@ export function debounce(wait = 300) {
   let timeout: any;
 
   return wrapper((next, ..._args: any[]) => {
-    clearTimeout(timeout);
+    return new Promise<ReturnType<typeof next>>((resolve, reject) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(async () => {
+        try {
+          // @ts-ignore
+          resolve(await next());
+        } catch (e) {
+          reject(e);
+        }
+      }, wait);
+    });
+  });
+}
 
-    timeout = setTimeout(() => {
-      next();
-    }, wait);
+async function sleep(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
   });
 }
 
 export async function demo() {
   const debouncedLog = debounce(500)(console.log);
-
-  async function sleep(time: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time);
-    });
-  }
 
   // BEGIN
   debouncedLog("Hey there!");
@@ -37,4 +43,40 @@ export async function demo() {
   debouncedLog("Sike! Interrupted again");
 }
 
-demo();
+// Debounce with access to the return value
+export async function demo2() {
+  const id = <T>(x: T) => {
+    return x;
+  };
+
+  const data = [
+    {
+      message: "Hey there!",
+      sleep: 0,
+    },
+    {
+      message: "Interruptting!",
+      sleep: 501,
+    },
+
+    {
+      message: "Anyone there?",
+      sleep: 200,
+    },
+    {
+      message: "Sike! Interrupted again",
+      sleep: 10,
+    },
+  ];
+
+  const debouncedId = debounce(500)(id);
+
+  for (const item of data) {
+    debouncedId(item).then((x) => {
+      console.log("X!", x);
+    });
+    await sleep(item.sleep);
+  }
+}
+
+demo2();
