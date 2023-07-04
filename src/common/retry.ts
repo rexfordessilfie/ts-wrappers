c42625baa96b0e79431fc7bfa6322abb247462e6
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-04 02:58:33 -0400

    test: add tests for retry

--- src/common/retry.ts
+++ src/common/retry.ts
@@ -1,18 +1,15 @@
-export const retry = (times: number, delay = 500) => {
+export const retry = (times: number, delay = 0) => {
   return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) =>
     function (...args: Parameters<typeof fn>) {
       let attempts = 0;
-      return new Promise<ReturnType<typeof fn>>((resolve, reject) => {
-        const interval = setInterval(() => {
-          attempts += 1;
+      return new Promise<Awaited<FReturn>>((resolve, reject) => {
+        const interval = setInterval(async () => {
+          attempts++;
           try {
             // @ts-ignore TS2683
-            const result = fn.apply(this, args);
+            const result = await fn.apply(this, args);
             resolve(result);
-
-            if (attempts <= times) {
-              clearInterval(interval);
-            }
+            clearInterval(interval);
           } catch (e) {
             if (attempts >= times) {
               reject(e);
---
+++ test/retry.test.ts
@@ -0,0 +1,102 @@
+import test from "ava";
+import { retry } from "../src";
+
+test("maintains reference to this", async (t) => {
+  const data = {
+    val: 0,
+    retryedInc: retry(3)(function (this: any) {
+      return ++this.val;
+    }),
+  };
+
+  await data.retryedInc();
+
+  t.is(data.val, 1);
+});
+
+test("executes when no errors", async (t) => {
+  let val = 0;
+
+  const retryedInc = retry(3)(() => {
+    return ++val;
+  });
+
+  await retryedInc();
+
+  t.is(val, 1);
+});
+
+test("retries with delay", async (t) => {
+  let currTime: number, prevTime: number;
+
+  const DELAY = 100;
+  const RETRY_AMOUNT = 3;
+
+  const retryedInc = retry(
+    RETRY_AMOUNT,
+    DELAY
+  )(() => {
+    [currTime, prevTime] = [performance.now(), currTime];
+
+    if (prevTime) {
+      const duration = currTime - prevTime;
+      t.assert(Math.round(duration / 10) * 10 >= DELAY);
+    }
+
+    throw new Error("Error!");
+  });
+
+  await retryedInc().catch(() => {});
+});
+
+test("retries and succeeds", async (t) => {
+  let val = 0;
+
+  const DELAY = 200;
+  const RETRY_AMOUNT = 3;
+
+  let callCount = 0;
+
+  const retryedInc = retry(
+    RETRY_AMOUNT,
+    DELAY
+  )(() => {
+    callCount += 1;
+
+    // Only succeed on final retry
+    if (callCount < 3) {
+      throw new Error("Error!");
+    }
+
+    return ++val;
+  });
+
+  await retryedInc();
+
+  t.is(val, 1);
+  t.is(callCount, 3);
+});
+
+test("retries and fails", async (t) => {
+  let val = 0;
+
+  let callCount = 0;
+
+  const RETRY_AMOUNT = 3;
+
+  const retryedInc = retry(RETRY_AMOUNT)(() => {
+    callCount += 1;
+
+    // Only succeed on final retry
+    if (callCount < RETRY_AMOUNT + 1) {
+      throw new Error("Error!");
+    }
+
+    return ++val;
+  });
+
+  await t.throwsAsync(async () => retryedInc());
+
+  t.is(val, 0);
+  t.is(callCount, 3);
+});
