6da3ccb8b30cab1cddecaa86a6333b87f786d9a7
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-04 03:24:59 -0400

    refactor: change retry to use loop

--- src/common/retry.ts
+++ src/common/retry.ts
@@ -1,22 +1,23 @@
+async function sleep(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 export const retry = (times: number, delay = 0) => {
   return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) =>
-    function (...args: Parameters<typeof fn>) {
-      let attempts = 0;
-      return new Promise<Awaited<FReturn>>((resolve, reject) => {
-        const interval = setInterval(async () => {
-          attempts++;
-          try {
-            // @ts-ignore TS2683
-            const result = await fn.apply(this, args);
-            resolve(result);
-            clearInterval(interval);
-          } catch (e) {
-            if (attempts >= times) {
-              reject(e);
-              clearInterval(interval);
-            }
+    async function (...args: Parameters<typeof fn>) {
+      for (let attempt = 1; attempt <= times; attempt++) {
+        try {
+          // @ts-ignore TS2683
+          const result = await fn.apply(this, args);
+          return result;
+        } catch (e) {
+          if (attempt >= times) {
+            throw e;
           }
-        }, delay);
-      });
+          await sleep(delay);
+        }
+      }
     };
 };
