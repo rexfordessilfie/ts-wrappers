3f6617cbd7083109df849427eeeb380f965d2130
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-03 23:38:12 -0400

    chore: await interceptor function

--- src/common/intercept.ts
+++ src/common/intercept.ts
@@ -1,10 +1,12 @@
-export function intercept<F extends Function>(interceptor: F) {
-  return function <FArgs extends any[], FReturn>(
+export function intercept<IArgs extends any[], IReturn>(
+  interceptor: (...args: IArgs) => IReturn
+) {
+  return function <FArgs extends IArgs, FReturn>(
     fn: (...args: FArgs) => FReturn
   ) {
-    return function (...args: Parameters<typeof fn>) {
+    return async function (...args: Parameters<typeof fn>) {
       // @ts-ignore TS2683
-      interceptor.apply(this, args);
+      await interceptor.apply(this, args);
       // @ts-ignore TS2683
       return fn.apply(this, args);
     };
