ed490965bda4cd9be1dc06545fd37f8f91291fef
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-05 01:08:24 -0400

    test: add repeat tests (#10)
    * test: add tests for repeat
    
    * feat: add sleep helper
    
    * feat: use iteration for repeat wrapper
    
    * fix: ensure repeat mainains function properties
    
    * chore: remove unused result variable

--- src/common/repeat.ts
+++ src/common/repeat.ts
@@ -1,14 +1,26 @@
-export const repeat = (times: number, delay = 500) => {
-  return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) =>
-    function (...args: Parameters<typeof fn>) {
-      let numTimes = 0;
-      const interval = setInterval(() => {
-        numTimes++;
-        if (numTimes >= times) {
-          clearInterval(interval);
+import { sleep } from "../sleep";
+
+export const repeat = (times: number, delay = 0) => {
+  return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) => {
+    async function newFn(...args: Parameters<typeof fn>) {
+      for (let i = 0; i < times; i++) {
+        try {
+          // @ts-ignore TS2683
+          await fn.apply(this, args);
+        } catch (e) {
+          console.error(e);
         }
-        // @ts-ignore TS2683
-        fn.apply(this, args);
-      }, delay);
-    };
+
+        await sleep(delay);
+      }
+    }
+
+    // Copy properties to new function
+    Object.entries(Object.getOwnPropertyDescriptors(fn)).forEach(
+      ([prop, descriptor]) => {
+        Object.defineProperty(newFn, prop, descriptor);
+      }
+    );
+    return newFn;
+  };
 };
