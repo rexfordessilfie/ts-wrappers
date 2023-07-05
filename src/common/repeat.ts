f494120e8c85d026b8c3d94bfea36fc62b393ff6
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-05 01:00:19 -0400

    chore: remove unused result variable

--- src/common/repeat.ts
+++ src/common/repeat.ts
@@ -4,11 +4,9 @@ export const repeat = (times: number, delay = 0) => {
   return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) => {
     async function newFn(...args: Parameters<typeof fn>) {
       for (let i = 0; i < times; i++) {
-        let result = undefined;
-
         try {
           // @ts-ignore TS2683
-          result = await fn.apply(this, args);
+          await fn.apply(this, args);
         } catch (e) {
           console.error(e);
         }
