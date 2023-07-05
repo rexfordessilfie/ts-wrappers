dee5225c261a16512c5bc8931046ff05ecf50a7d
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-05 00:54:48 -0400

    feat: use iteration for repeat wrapper

--- src/common/repeat.ts
+++ src/common/repeat.ts
@@ -1,14 +1,22 @@
-export const repeat = (times: number, delay = 500) => {
-  return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) =>
-    function (...args: Parameters<typeof fn>) {
-      let numTimes = 0;
-      const interval = setInterval(() => {
-        numTimes++;
-        if (numTimes >= times) {
-          clearInterval(interval);
+import { sleep } from "../sleep";
+
+export const repeat = (times: number, delay = 0) => {
+  return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) => {
+    async function newFn(...args: Parameters<typeof fn>) {
+      for (let i = 0; i < times; i++) {
+        let result = undefined;
+
+        try {
+          // @ts-ignore TS2683
+          result = await fn.apply(this, args);
+        } catch (e) {
+          console.error(e);
         }
-        // @ts-ignore TS2683
-        fn.apply(this, args);
-      }, delay);
-    };
+
+        await sleep(delay);
+      }
+    }
+
+    return newFn;
+  };
 };
