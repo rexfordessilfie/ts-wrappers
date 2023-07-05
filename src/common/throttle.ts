74f3735dce999576ded0928934ecd4fffd6364cf
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-05 01:16:19 -0400

    fix: ensure throttle maintains function properties

--- src/common/throttle.ts
+++ src/common/throttle.ts
@@ -1,20 +1,36 @@
 export function throttle(delay: number) {
   return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) => {
     let wait = false;
-    return function (...args: Parameters<typeof fn>) {
+    async function newFn(...args: Parameters<typeof fn>) {
       if (wait) {
         return;
       }
 
-      // @ts-ignore TS2683
-      const result = fn.apply(this, args);
       wait = true;
 
-      setTimeout(() => {
-        wait = false;
-      }, delay);
+      let result: FReturn | undefined = undefined;
+
+      try {
+        // @ts-ignore TS2683
+        result = await fn.apply(this, args);
+      } catch (e) {
+        throw e;
+      } finally {
+        setTimeout(() => {
+          wait = false;
+        }, delay);
+      }
 
       return result;
-    };
+    }
+
+    // Copy properties to new function
+    Object.entries(Object.getOwnPropertyDescriptors(fn)).forEach(
+      ([prop, descriptor]) => {
+        Object.defineProperty(newFn, prop, descriptor);
+      }
+    );
+
+    return newFn;
   };
 }
