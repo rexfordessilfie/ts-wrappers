d068e0869f8776080632d3f046c3087e0e72d293
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-05 01:27:56 -0400

    fix: ensure trace maintains function properties

--- src/common/trace.ts
+++ src/common/trace.ts
@@ -1,11 +1,21 @@
 export const trace = <FArgs extends any[], FReturn>(
   fn: (...args: FArgs) => FReturn
-) =>
-  function (...args: Parameters<typeof fn>) {
+) => {
+  function newFn(...args: Parameters<typeof fn>) {
     const tag = `${fn.name}(${args}) | duration`;
     console.time(tag);
     // @ts-ignore TS2683
     const result = fn.apply(this, args);
     console.timeEnd(tag);
     return result;
-  };
+  }
+
+  // Copy properties to new function
+  Object.entries(Object.getOwnPropertyDescriptors(fn)).forEach(
+    ([prop, descriptor]) => {
+      Object.defineProperty(newFn, prop, descriptor);
+    }
+  );
+
+  return newFn;
+};
