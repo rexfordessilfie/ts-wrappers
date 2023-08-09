0982ddd62272aa63254efc23d39932c1df11a98a
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-08-09 03:41:59 -0400

    feat: add keyed debounce

--- src/common/debounce.ts
+++ src/common/debounce.ts
@@ -1,22 +1,51 @@
-export function debounce(wait = 300, leading = false) {
-  let timeout: any;
-  return <FArgs extends any[], FReturn>(fn: (...args: FArgs) => FReturn) => {
-    function newFn(...args: Parameters<typeof fn>) {
-      return new Promise<Awaited<ReturnType<typeof fn>>>(
-        async (resolve: any, reject: any) => {
-          clearTimeout(timeout);
+type DebounceOptions<KArgs extends any[]> = {
+  leading?: boolean;
+  keyFn?: (...args: KArgs) => any;
+};
 
-          // @ts-ignore TS2683
-          leading && !timeout && resolve(await fn.apply(this, args));
+export function debounce<KArgs extends any[]>(
+  wait: number,
+  options?: DebounceOptions<KArgs>
+) {
+  const { leading = false, keyFn } = options || {};
+
+  let timeout: any;
+  let timeoutKey: any;
+  let timeoutCache: Record<string, any>;
+
+  if (keyFn) {
+    timeoutCache = {};
+  }
+  return <FArgs extends KArgs, FReturn>(fn: (...args: FArgs) => FReturn) => {
+    function newFn(...args: Parameters<typeof fn>) {
+      if (keyFn) {
+        // @ts-ignore TS2683
+        timeoutKey = keyFn.apply(this, args);
+        timeout = timeoutCache[timeoutKey];
+      }
+      return new Promise<Awaited<ReturnType<typeof fn>>>(
+        (resolve: any, reject: any) => {
+          if (leading && !timeout) {
+            // @ts-ignore TS2683
+            (async () => resolve(await fn.apply(this, args)))();
+          }
+
+          clearTimeout(timeout);
           timeout = setTimeout(async () => {
             try {
               timeout = null;
-              // @ts-ignore TS2683
-              !leading && resolve(await fn.apply(this, args));
+              if (!leading) {
+                // @ts-ignore TS2683
+                resolve(await fn.apply(this, args));
+              }
             } catch (e) {
               reject(e);
             }
           }, wait);
+
+          if (keyFn) {
+            timeoutCache[timeoutKey] = timeout;
+          }
         }
       );
     }
