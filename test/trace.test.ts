367341802da8343cbaf2a1df703f7db31646d049
Rexford Essilfie <rexfordessilfie09@gmail.com>
2024-02-13 20:32:35 -0800

    chore: move declarations to trace demo file

--- test/trace.test.ts
+++ test/trace.test.ts
@@ -1,10 +1,5 @@
 import test from "ava";
 import { trace } from "../src";
-import type { Any } from "src/common/types";
-
-
-declare function myFn<Args extends Any.Array>(arg_0: Args): string | number
-declare function myFn<T, U>(arg_0: T, arg_1: U): T | U
 
 test("maintains reference to this", async (t) => {
   const data = {
