d7716cbfab815d931ac6fe47333e0fd383b1b9b2
Rexford Essilfie <rexfordessilfie09@gmail.com>
2024-08-14 22:45:43 -0700

    fix: explicit wrapper type (#28)
    * chore: update type plus fix typo
    
    * fix: short-circuit function types
    
    * chore: add prettierrc
    
    * fix: awaited wrapped promise
    
    * chore: improve strategy for excluding sentinel
    
    * chore: simplify complex type
    
    * 0.0.3-alpha.0
    
    * chore: cleanup
    
    * 0.0.3-alpha.1
    
    * 0.0.3

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,5 +1,5 @@
 export default function wrapper<CArgs extends any[], CReturn>(
-  cb: (fn: Fn, ...args: CArgs) => CReturn
+  cb: (fn: Fn<FnReturnType, CArgs>, ...args: CArgs) => CReturn
 ) {
   return <FArgs extends CArgs, FReturn>(func: Func<FArgs, FReturn>) => {
     return function (...args: Parameters<typeof func>) {
@@ -8,12 +8,7 @@ export default function wrapper<CArgs extends any[], CReturn>(
         this,
         func as Parameters<typeof cb>[0],
         ...(args as CArgs)
-      ) as Replace<
-        CReturn,
-        FnReturnType,
-        ReturnType<typeof func>,
-        FnReturnType
-      >;
+      );
     };
   };
 }
@@ -25,39 +20,12 @@ type Brand<T, TBrand extends string> = T & {
 };
 
 export type Func<Args extends any[], Return> = (...args: Args) => Return;
-type FnReturnBrand = "__FUNC_RETURN__";
+type FnReturnBrand = "__fn_return__";
 type FnReturnType = Brand<{}, FnReturnBrand>;
 
-type ApplyBrand<T, TBrand extends string> = T extends TBrand
-  ? T
-  : Brand<T, TBrand>;
-
-type Fn<ReturnType = FnReturnType> = <
-  T = ReturnType,
-  Args extends any[] = any[]
+type Fn<ReturnType = FnReturnType, ArgsType extends any[] = any[]> = <
+  Return = ReturnType,
+  Args extends any[] = ArgsType
 >(
   ...args: Args
-) => ApplyBrand<T, FnReturnBrand>;
-
-type IsEqual<A, B> = A extends B ? (B extends A ? true : false) : false;
-type LeftIfNotEqual<A, B> = IsEqual<A, B> extends false ? A : never;
-type Spreadable<T> = T extends any[] ? T : never;
-
-type Replace<Source, A, B, Sentinel = never> = Source extends Sentinel
-  ? Omit<Source, keyof Sentinel> extends infer T
-  ? Replace<LeftIfNotEqual<T, {}>, A, B, Sentinel> extends infer U
-  ? U | B
-  : never // Dummy ternary just so we can get an alias T
-  : never // Dummy ternary just so we can get an alias U
-  : Source extends Promise<infer T>
-  ? Promise<Replace<T, A, B, Sentinel>>
-  : Source extends [infer AItem, ...infer ARest]
-  ? [
-    Replace<AItem, A, B, Sentinel>,
-    ...Spreadable<Replace<ARest, A, B, Sentinel>>
-  ]
-  : Source extends Record<string | number | symbol, any>
-  ? { [K in keyof Source]: Replace<Source[K], A, B, Sentinel> }
-  : Source extends A
-  ? Exclude<Source, A> | B
-  : Exclude<Source, A>;
+) => Return;
